04ea2837da05c80a94591af676ce99366454a1b2
Michael Hammerl <michael.hammerl@stud.th-rosenheim.de>
2025-01-08 10:51:28 +0100

    updated matlab file to latest lecture version

--- drm_simulation_students.m
+++ drm_simulation_students.m
@@ -1,4 +1,4 @@
-%% stOFDM Workshop: Lesson One
+%% OFDM Development Enviroment
 clc
 close all
 clear all
@@ -53,8 +53,12 @@ SlkTemp = [SlkTemp(:,end-stOFDM.iNg+1:end) SlkTemp];
 SlkTemp = SlkTemp.';
 vfcTransmitSignal = SlkTemp(:);
 
+% Repeat iG=4 Frames
+iG = 4;
+vfcTransmitSignal = repmat(vfcTransmitSignal,iG,1);
+
 %% Channel
-% Switch Cahnnel
+% Switch Channels
 iSwitchChannel = 1;
 
 switch iSwitchChannel
@@ -63,14 +67,36 @@ switch iSwitchChannel
         vfcReceiveSignal = vfcTransmitSignal;
 
     case 1 % simulated channel
+
         stChannel = initChannel();
-        vfcReceiveSignal = channel_sim(vfcTransmitSignal,stChannel,stOFDM);
+        % vfcReceiveSignal = channel_sim(vfcTransmitSignal,stChannel,stOFDM);
+
+        % Simulate iSampleShift = 17 between Tx and Rx
+        iSampleShift = 0;
+        vfcTransmitSignal = circshift(vfcTransmitSignal,[iSampleShift 0]);
+
+        
+        % Frequency- and Phase-Offset
+        vfcPhaser = exp(j*stChannel.fOmegaOffset*[0:length(vfcTransmitSignal)-1]+j*stChannel.fPhaseOffset);
+        vfcPhaser = vfcPhaser(:);
+        vfcReceiveSignal = vfcTransmitSignal.*vfcPhaser;
+
+        % sample offset
+        vfcFFT = fft(vfcReceiveSignal);
+        vOmega = 2*pi*[0:length(vfcFFT)-1].'/length(vfcFFT);
+        vfcReceiveSignal = ifft(vfcFFT.*fftshift(exp(j*vOmega*stChannel.fSampleOffset)));
+
+        % AWGN Channel
+        vfcReceiveSignal = awgn(vfcReceiveSignal,stChannel.fSNRdB);
+
+        %Multipath channel
+        vfcReceiveSignal = conv(vfcReceiveSignal,stChannel.vfImpulseResponse);
 
     case 2 % capture with TenTec
         vfcReceiveSignal = captureDRM(2) ;
 
     case 3 % use Adalm Pluto for TX and RX
-       stAdalmPluto = initSDR();
+       stAdalmPluto = initSDR('USB');
        vfcReceiveSignal = LoopbackAdalmPluto(vfcTransmitSignal,stAdalmPluto);
 
     case 4 % use ZedBoard for TX and RX
@@ -78,12 +104,80 @@ switch iSwitchChannel
        vfcReceiveSignal = LoopbackZedBoard(vfcTransmitSignal,stZedboard);
        vfcReceiveSignal =vfcReceiveSignal(:,1);
 
+    case 5 % Use Sattelite Communication Toolsbox for Channel Simulation
+
 end
 %% Detect Robustness Mode
+% force vfcReceiveSignal to be a column vector
+vfcReceiveSignal = vfcReceiveSignal(:);
 
+figure(100)
+plot(-length(vfcReceiveSignal)+1:length(vfcReceiveSignal)-1,abs(xcorr((vfcReceiveSignal))))
+xlabel('\Delta k')
+ylabel('r_{xx}(\Delta k)')
 
+% Detect Robustness by autocorrelation at dk = [288 256 176 112]
+% Autocorrelation matrix at dk corresponding to FFT lengths
+mX(1,:) = circshift(vfcReceiveSignal,[288 0])';
+mX(2,:) = circshift(vfcReceiveSignal,[256 0])';
+mX(3,:) = circshift(vfcReceiveSignal,[176 0])';
+mX(4,:) = circshift(vfcReceiveSignal,[112 0])';
+
+% Calculate Robustnes Mode
+[~, iModeEst] = max(abs(mX*vfcReceiveSignal));
+iOcc = 3;
+% Determine OFDM Parameters for estimated Robustnes Mode
+% FFT length 
+iNfft = get_drm_n_useful(iModeEst,3); % Occupancy fixed at 3
+% Guard Intervall Length
+iNg = get_drm_n_guard(iModeEst,3);% Occupancy fixed at 3
+% Complete Symbol length
+iNs = iNfft + iNg;
+% Number of Symols per DRM Frame
+iNOfSymbolsPerFrame = get_drm_symbols_per_frame(iModeEst);
 %% Synchronization
+% Number of Symbols in CaptureBuffer
+iNOfSymbols = floor(length(vfcReceiveSignal)/iNs);
 
+% Timing Recovery with CP
+vIndexGI = [0:iNg-1].';
+R = zeros(1,iNs);
+
+for l = 0:iNOfSymbols-2
+for k = 1:iNs
+
+    R(k) = R(k) + ...
+                        sum(conj(vfcReceiveSignal(k+vIndexGI+l*iNs)).*vfcReceiveSignal(k+vIndexGI+iNfft+l*iNs));
+
+    % R(k) = R(k) + ...
+    %                     vfcReceiveSignal(k+vIndexGI+l*iNs)'*vfcReceiveSignal(k+vIndexGI+iNfft+l*iNs);
+end
+end
+
+[~ ,iStartSample] = max(abs(R));
+dOmegaEst = angle(R(iStartSample))/iNfft;
+
+% Compensate Frequemncy Offset
+vfcPhaser = exp(-j*dOmegaEst*[0:length(vfcReceiveSignal)-1]);
+vfcPhaser = vfcPhaser(:);
+%vfcReceiveSignal = vfcReceiveSignal .* vfcPhaser;
+
+% Adjust to Start Sample
+vfcReceiveSignal = vfcReceiveSignal(iStartSample:end);
+iNOfSymbols = floor(length(vfcReceiveSignal)/iNs);
+vfcReceiveSignal = vfcReceiveSignal(1:iNOfSymbols*iNs);
+
+
+
+figure(101)
+plot([0:iNs-1],abs(R))
+xlabel('k')
+ylabel('R_{xy}(k)')
+hold on
+plot(iStartSample, abs(R(iStartSample)),'ro')
+hold off
+
+%figure;plot(abs(sum(reshape(filter(ones(1,iNg),1,vfcReceiveSignal.*conj(circshift(vfcReceiveSignal,[iNfft 0]))),iNs,iNOfSymbols),2)))
 
 %% OFDM Demodulator
 % Serial to Parallel Conversion
@@ -96,17 +190,170 @@ RlkTemp = RlkTemp(:,stOFDM.iNg+1:end);
 Rlk = fftshift(fft(RlkTemp,stOFDM.iNfft,2),2);
 
 %% Frame Detection
+% Generate Pilots
+Plk = get_drm_pilot_frame(iModeEst,3);
+% Calczlate Correlation Metrik
+Mlk = xcorr2(Rlk,Plk);
+Mlk(1:iNOfSymbolsPerFrame-1,:) = [];
+figure(201);
+subplot(2,1,1)
+mesh([0:2*iNfft-2]-iNfft,0:size(Mlk,1)-1,abs(Mlk))
 
+xlabel('k Subchannel')
+ylabel('l Symbols')
+zlabel('|M(l,k)|')
+
+subplot(2,1,2)
+plot(0:size(Mlk,1)-1,abs(Mlk(:,iNfft)))
+xlabel('l Symbols')
+ylabel('|M(l,0)|')
+
+iNOfFrames = floor(size(Mlk,1)/iNOfSymbolsPerFrame);
+
+[~ , viFrameStart] = maxk(abs(Mlk(:,iNfft)),iNOfFrames);
+
+viFrameStart = sort(viFrameStart);
+viFrameStart(end)=[];
+
+viFrameSymbols = 0:iNOfSymbolsPerFrame-1;
+
+for iFrame = 1:length(viFrameStart)
+
+    stRlk{iFrame} = Rlk(viFrameStart(iFrame)+viFrameSymbols,:);
+end
+
+% Run first Frame
+Rlk = stRlk{1};
 
 %% Fine Synchronization
 
+figure(301)
+mPhase = angle(conj(Rlk).*Plk);
+%mPhase = angle(conj(Rlk).*Slk);
 
+mesh([-iNfft/2:iNfft/2-1],[1:size(Plk,1)],unwrap(mPhase))
+ylabel('Symbol l')
+xlabel('Subchannel k')
+zlabel('Phase \Phi(l,k)')
+
+for l = 1:size(Plk,1)
+figure(302)
+stem([-iNfft/2:iNfft/2-1],unwrap(mPhase(l,:)))
+xlabel('Subchannel k')
+ylabel(['Phase \Phi(' num2str(l) ',k)'])
+
+
+% Linear Regression of Phases
+kPilots = [find(Plk(l,:) ~= 0)-iNfft/2-1].';
+V = [kPilots ones(size(kPilots))];
+vPhi = unwrap(mPhase(l,Plk(l,:) ~= 0)).';
+% tic;m = inv(V'*V)*V'*vPhi;toc
+% tic;m = pinv(V)*vPhi;toc
+m = V\vPhi;
+
+
+hold on
+kAxis = -iNfft/2:iNfft/2-1;
+plot(kAxis,m(1)*kAxis+m(2))
+hold off
+
+% Comensate Phase correction
+mPhaseEst = m(1)*kAxis+m(2);
+%Rlk(l,:) = Rlk(l,:) .* exp(-j*mPhaseEst);
+end
+
+% Alternative
+mPhase = conj(Rlk).*Plk;
+mPhase = mPhase(:,[16 48 64]+iNfft/2+1);
+
+figure(303)
+subplot(2,1,1)
+stem(1:15,angle(mPhase))
+xlabel('symbol l')
+ylabel(['Phase \Phi(l,k=[' num2str([16 48 64]) '])'])
+
+mPhaseFFT = fft(mPhase,1024,1);
+
+subplot(2,1,2)
+stem(abs(mPhaseFFT))
+xlabel('\mu')
+ylabel(['FFT\{\Phi(l,k=[' num2str([16 48 64]) '])\}'])
 %% Kanalschätzung und Entzerrung
+dc = get_drm_dc_position(iModeEst,iOcc);
+kmin = get_drm_kmin(iModeEst,iOcc)+dc;
+kmax = get_drm_kmax(iModeEst,iOcc)+dc;
+kInterpolate = kmin:kmax;
 
+for l=1:size(Plk,1)
+    
+    cInterpolater = 'Wiener';
+    
+    switch cInterpolater
+        case 'Spline'
+            % Channel Estimation -- symbol by symbol
+            kIndex = find(Plk(l,:) ~= 0);
+            Hk = Rlk(l,kIndex)./Plk(l,kIndex);
+            Hint = zeros(1,stOFDM.iNfft);
+            Hint(kInterpolate) = interp1(kIndex,Hk,kInterpolate);
+            
+            % Equalization -- symbol by symbol
+            Rlk(l,kInterpolate) = Rlk(l,kInterpolate)./Hint(kInterpolate);
+            
+        case 'Wiener'
+            
+            % Korrelations Matrix R
+            kIndex = find(Plk(l,:) ~= 0);
+            mIndex = [];
+            for k=kIndex
+                mIndex = [mIndex ; kIndex-k];
+            end
+            Omega_g = 0.3*1/mean(diff(kIndex));
+            Omega_g = 0.5*1/stOFDM.iNg;
+            R_ = sinc(mIndex*Omega_g);
+            SNR = 50;
+            N = 10^(-SNR/10);
+            R = R_+eye(size(R_,1))*N;
+            
+            Hint = zeros(1,stOFDM.iNfft);
+            Hk = Rlk(l,kIndex)./Plk(l,kIndex);
+            Hk = Hk(:);
+            for k = kInterpolate
+                
+                % Observation Vector b
+                vIndex = kIndex - k;
+                b = sinc(vIndex*Omega_g);
+                b = b(:);
+                
+                % Wiener Filter g
+                g = inv(R)*b;
+                
+                % Apply Wiener Filter
+                Hint(k) = g.' * Hk;
+                
+            end
+
+
+            if SwitchDemoSync
+            figure(10)
+            stem(abs(Hint))
+            hold on
+            plot(kIndex,abs(Hk),'ro')
+            hold off
+            end
+
+
+            % Eqialization
+            Rlk(l,kInterpolate) = Rlk(l,kInterpolate)./Hint(kInterpolate);
+            
+    end
+    Hint(kInterpolate) = Hint(kInterpolate).*hamming(1,length(kInterpolate));
+    h_est = ifft( fftshift(Hint));
+    
+end
 
 
 %% Graphical Output
-
+SlkTemp = get_drm_data_template_frame(stDRM.mode, stDRM.occupancy);
 figure(1)
 subplot(2,2,1)
 imagesc((1:stOFDM.iNfft)-get_drm_dc_position(stDRM.mode,stDRM.occupancy),0:size(Slk,1)-1,abs(Slk))
@@ -115,7 +362,7 @@ ylabel('Symbol l')
 title('Transmit Frame |Slk|')
 
 subplot(2,2,2)
-plot(Slk(Plk ==0),'r.')
+plot(Slk(SlkTemp ==1),'r.')
 grid
 axis square
 xlabel('I')
@@ -129,9 +376,9 @@ ylabel('Symbol l')
 title('Receive Frame |Rlk|')
 
 subplot(2,2,4)
-plot(Rlk(Plk ==0),'r.')
+plot(Rlk(SlkTemp ==1),'r.')
 grid
-fLimit = max(max(abs(Rlk(Plk ==0))));
+fLimit = max(max(abs(Rlk(SlkTemp ==1))));
 axis square
 axis([-fLimit fLimit -fLimit fLimit])
 
